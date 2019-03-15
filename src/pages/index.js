8e96981db2a417749bae5571be92e2ff21b073f6
Ian Sinnott <ian@iansinnott.com>
2019-03-15 17:30:33 +0800

    Move lib files to avoid extra pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
-import lookup from './lookup.js';
+import lookup from '../components/lookup.js';
 
 const Form = props => {
   return (
