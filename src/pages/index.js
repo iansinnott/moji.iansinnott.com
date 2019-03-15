17fccbbac9db4aa4d4e1331b7f8937eb34e3d2ea
Ian Sinnott <ian@iansinnott.com>
2019-03-15 16:53:56 +0800

    Build initial site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,52 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import lookup from './lookup.js';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Form = props => {
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        value={props.value}
+        type='text'
+        placeholder='Emoji!'
+        onChange={e => props.onChange(e.target.value)}
+      />
+    </form>
+  );
+};
 
-export default IndexPage
+class IndexPage extends React.Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = value => {
+    this.setState(() => ({ value }));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <SEO title='Home' keywords={[`chinese`, `emoji`]} />
+        <h1>Emoji in Chinese!</h1>
+        <h3>What...?</h3>
+        <p>
+          The Chinese keyboards on iOS are great for inserting emoji into text.
+          But how do you say those emotions in Chinese? Enter an emoji and find
+          out!
+        </p>
+        <Form onChange={this.handleChange} value={this.state.value} />
+        <div style={{ paddingBottom: '1em' }} className='emoji'>
+          {lookup(this.state.value).map(x => (
+            <div key={x} class="item">{x}</div>
+          ))}
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default IndexPage;
